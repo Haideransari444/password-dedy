b6d342d545755a7001abed4232d82963b357eb08
Haider <muzamilhaider444@gmail.com>
2025-07-10 01:42:24 +0500

    fahd mustufa inspo

--- passworddedy/passworddedy/settings.py
+++ passworddedy/passworddedy/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'passworddedy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["temlates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
